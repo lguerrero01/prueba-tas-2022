b0e3c503af3ae6fc67f9c418cc78aca852eeb490
Alejandro <luisguerrero12387@gmail.com>
2022-01-06 20:47:29 -0400

    adding routing and minor fix

--- webApp/src/app/app-routing.module.ts
+++ webApp/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { HeaderComponent } from './layout/header/header.component';
+import { StoreComponent } from './pages/store/store.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '', 
+    component: HeaderComponent
+  },
+  {
+    path: 'store', 
+    component: StoreComponent
+  },
+  {
+    path: "**",
+    redirectTo: "",
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { StoreComponent } from './pages/store/store.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    StoreComponent,
+    ProductsComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
